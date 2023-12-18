b6fc4a08278705511a55ed2e78a1ab5855398e40
luhut384 <145972339+luhut384@users.noreply.github.com>
2023-12-18 08:10:02 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,61 @@
+// ini variabel dengan let
+let nama = "luthhud maulana"
+console.log(nama)
+
+// ini variabel dengan const
+const firstname = "luthhud"
+const lastname = "maulana"
+console.log(firstname + lastname)
+
+// ini tipe data int
+const age = 21
+console.log(age)
+console.log(typeof age)
+
+// ini tipe data string
+const gender = "lk"
+console.log(typeof gender)
+
+//  ini tipe data bolean
+const isMarried = false
+console.log(isMarried)
+console.log(typeof isMarried)
+
+// operator aritmatika
+const x = 9
+const y = 8
+
+//pertambahan
+const z = x + y
+
+//pengurangan
+const m = x - y
+
+//perkalian
+const n = y * x
+
+//pembagian
+const o = x / y
+
+//pembagian
+const c = 10 % 2
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(o)
+console.log(c)
+
+// function
+function salam() {
+    alert("assalamualaikum")
+}
+
+salam();
+
+function isTrue() {
+    let city = "Depok"
+}
+console.log(city)
+
+isTrue
